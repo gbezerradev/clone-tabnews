227e7edb656114f6df2095a94dd7631b03731d55
Guilherme Bezerra <70481718+sudoguilherme@users.noreply.github.com>
2023-06-23 14:55:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do https://tabnews.com.br para acompanhamento do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bom dia, b. Passando só pra lembrar que eu te amo muito! 🫀</h1>
+  return <h1>Bom dia, b. Passando só pra lembrar que eu te amo muito! 🫀</h1>;
 }
 
 export default Home;
