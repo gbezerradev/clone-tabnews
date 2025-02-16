46a99190027c4de1a45f7bbbaf7a40fbd7fc005e
Guilherme Bezerra <70481718+gbezerradev@users.noreply.github.com>
2025-02-16 00:50:29 +0000

    feat: extend error handling with ServiceError and MethodNotAllowedError classes

--- infra/errors.js
+++ infra/errors.js
@@ -1,12 +1,52 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno ocorreu", {
       cause,
     });
 
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento", {
+      cause,
+    });
+
+    this.name = "ServiceError";
+    this.action = "Tente novamente mais tarde";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método POST não permitido para este endpoint");
+
+    this.name = "MethodNotAllowedError";
+    this.action = "Verique o método HTTP utilizado na requisição";
+    this.statusCode = 405;
   }
 
   toJSON() {
