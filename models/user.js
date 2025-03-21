4fcf0f7f0b1ab2c755ea99d6b52c264daf24761c
Guilherme Bezerra <70481718+gbezerradev@users.noreply.github.com>
2025-03-21 16:19:41 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
 
 async function create({ username, email, password }) {
   await checkIfEmailIsAlreadyInUse(email);
@@ -71,8 +71,39 @@ async function create({ username, email, password }) {
   }
 }
 
+async function findOneByUsername(username) {
+  const userRecord = await runSelectQuery(username);
+  return userRecord;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (result.rows.length === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado",
+        action: "Verifique se o username está correto",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
